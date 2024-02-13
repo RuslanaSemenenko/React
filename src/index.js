937d545f039321f4809e4bae5dc1a1ec1e946956
Ruslana Semenenko <katotochkaaa@gmail.com>
2024-02-13 14:08:07 +0000

    work

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 import './index.css';
 
+
 ReactDOM.render(
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  
   document.getElementById('root')
 );
